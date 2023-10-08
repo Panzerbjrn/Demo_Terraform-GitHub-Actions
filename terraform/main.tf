a91dcd14e43f2f62b5c13c1f888e38239b413f8f
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2023-10-08 21:58:44 +0200

    Refreshing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfrg"
-    storage_account_name = "tfsa"
-    container_name       = "tfcont"
-    key                  = "terraformgithubexample.tfstate"
-  }
-}
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "tfrg"
+#    storage_account_name = "tfsa"
+#    container_name       = "tfcont"
+#    key                  = "terraformgithubexample.tfstate"
+#  }
+#}
+#
 
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. #
@@ -17,7 +18,7 @@ provider "azurerm" {
 #Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "TerraformGHActionsDemo"
-  location = var.azure_region
+  location = "westeurope"
   tags = {
     source = "TerraformGHActions"
   }
